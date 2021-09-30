1b837e5793be4319f89fc802cba29c93b2067e8a
Imran <90487346+ik1037@users.noreply.github.com>
2021-09-29 21:42:10 -0400

    Add files via upload

---
+++ week_03_assignment.R
@@ -0,0 +1,33 @@
+###' Week 03 assignment
+###' 
+###' Function to create a special "matrix" object that can cache its inverse.
+###' 
+makeCacheMatrix <- function(x = matrix()) {
+  inverse <- NULL                           
+  set <- function(y) {                     
+    x <<- y                             
+    inverse <<- NULL                        
+  }
+  get <- function() x                     
+  
+  setinverse <- function(inverse) inverse <<- inverse  
+  getinverse <- function() inverse                    
+  list(set = set, get = get, setinverse = setinverse, getinverse = getinverse)
+  
+}
+
+###' Function to compute the inverse of the special "matrix" returned by makeCacheMatrix
+###' above. If the inverse has already been calculated (and the matrix has not changed),
+###' then the cachesolve should retrieve the inverse from the cache.
+
+cachesolve <- function(x, ...) {
+  inverse <- x$getinverse()
+  if(!is.null(inverse)) {
+    message("getting cached data")
+    return(inverse)
+  }
+  data <- x$get()
+  inverse <- solve(data, ...)
+  x$setinverse(inverse)
+  inverse
+}
